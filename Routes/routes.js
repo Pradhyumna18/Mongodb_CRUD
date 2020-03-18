cb91974bed43811240f3598f003cde0881937daf
Pradhyumna18 <pradhyumna0204@gmail.com>
2020-03-18 10:56:42 +0530

    http method changed

--- Routes/routes.js
+++ Routes/routes.js
@@ -4,6 +4,6 @@ const router=express.Router();
 router.post('/create',controller.createData)
 router.get('/get',controller.getData)
 router.get('/get/:id',controller.getOneData)
-router.post('/update/:id',controller.updateOneData)
+router.put('/update/:id',controller.updateOneData)
 router.delete('/delete/:id',controller.deleteOneData)
 module.exports=router;
